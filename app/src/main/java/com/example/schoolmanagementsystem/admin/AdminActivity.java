9d43dceba29182fcd6a52eb2137f5117c52fa62c
hritick1 <hritick.12016375@lpu.in>
2022-07-02 17:21:20 +0530

    Empty Project added
    Added empty project to start making it

---
+++ app/src/main/java/com/example/schoolmanagementsystem/admin/AdminActivity.java
@@ -0,0 +1,15 @@
+package com.example.schoolmanagementsystem.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.schoolmanagementsystem.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
